49f12cb3ef7e08af53a7fe74bfdceb594f5cb54d
Maxwell Tavares Rocha <Maxwelltrt0709@gmail.com>
2021-04-17 18:36:09 -0400

    Commit
    el primero, y posbilemente el ultimo

---
+++ Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿
+namespace SteamKeyDeveloper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bunifuElipseForm = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.bunifuElipsePanel = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.hideFeedback = new System.Windows.Forms.Timer(this.components);
+            this.bunifuGradientPanel1 = new Bunifu.Framework.UI.BunifuGradientPanel();
+            this.feedbackpannel = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.tryanotherkey = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Key = new System.Windows.Forms.TextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.bunifuGradientPanel1.SuspendLayout();
+            this.feedbackpannel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bunifuElipseForm
+            // 
+            this.bunifuElipseForm.ElipseRadius = 20;
+            this.bunifuElipseForm.TargetControl = this;
+            // 
+            // bunifuElipsePanel
+            // 
+            this.bunifuElipsePanel.ElipseRadius = 25;
+            this.bunifuElipsePanel.TargetControl = this.panel1;
+            // 
+            // hideFeedback
+            // 
+            this.hideFeedback.Interval = 1500;
+            this.hideFeedback.Tick += new System.EventHandler(this.hideFeedback_Tick);
+            // 
+            // bunifuGradientPanel1
+            // 
+            this.bunifuGradientPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuGradientPanel1.BackgroundImage")));
+            this.bunifuGradientPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuGradientPanel1.Controls.Add(this.feedbackpannel);
+            this.bunifuGradientPanel1.Controls.Add(this.label2);
+            this.bunifuGradientPanel1.Controls.Add(this.pictureBox1);
+            this.bunifuGradientPanel1.Controls.Add(this.label1);
+            this.bunifuGradientPanel1.Controls.Add(this.button2);
+            this.bunifuGradientPanel1.Controls.Add(this.exitButton);
+            this.bunifuGradientPanel1.Controls.Add(this.tryanotherkey);
+            this.bunifuGradientPanel1.Controls.Add(this.panel1);
+            this.bunifuGradientPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bunifuGradientPanel1.GradientBottomLeft = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(46)))), ((int)(((byte)(74)))));
+            this.bunifuGradientPanel1.GradientBottomRight = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(46)))), ((int)(((byte)(74)))));
+            this.bunifuGradientPanel1.GradientTopLeft = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.bunifuGradientPanel1.GradientTopRight = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.bunifuGradientPanel1.Location = new System.Drawing.Point(0, 0);
+            this.bunifuGradientPanel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.bunifuGradientPanel1.Name = "bunifuGradientPanel1";
+            this.bunifuGradientPanel1.Quality = 10;
+            this.bunifuGradientPanel1.Size = new System.Drawing.Size(437, 270);
+            this.bunifuGradientPanel1.TabIndex = 0;
+            // 
+            // feedbackpannel
+            // 
+            this.feedbackpannel.Controls.Add(this.pictureBox3);
+            this.feedbackpannel.Controls.Add(this.pictureBox2);
+            this.feedbackpannel.Controls.Add(this.label3);
+            this.feedbackpannel.Location = new System.Drawing.Point(0, 1);
+            this.feedbackpannel.Name = "feedbackpannel";
+            this.feedbackpannel.Size = new System.Drawing.Size(437, 39);
+            this.feedbackpannel.TabIndex = 6;
+            this.feedbackpannel.Click += new System.EventHandler(this.feedbackpannel_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = global::SteamKeyDeveloper.Properties.Resources.icons8_checked_96px;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Enabled = false;
+            this.pictureBox2.Location = new System.Drawing.Point(306, 1);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(35, 36);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Enabled = false;
+            this.label3.Font = new System.Drawing.Font("Ubuntu", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Green;
+            this.label3.Location = new System.Drawing.Point(98, -6);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(243, 47);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Key in clipboard";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Ubuntu", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(100, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Pseudo";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::SteamKeyDeveloper.Properties.Resources.icons8_steam_52px;
+            this.pictureBox1.Location = new System.Drawing.Point(193, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Ubuntu", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(98, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 47);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Steam Key Generator";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(83)))), ((int)(((byte)(53)))), ((int)(((byte)(74)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(83)))), ((int)(((byte)(53)))), ((int)(((byte)(74)))));
+            this.button2.Location = new System.Drawing.Point(369, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(25, 25);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(69)))), ((int)(((byte)(69)))));
+            this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(69)))), ((int)(((byte)(69)))));
+            this.exitButton.Location = new System.Drawing.Point(400, 12);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(25, 25);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // tryanotherkey
+            // 
+            this.tryanotherkey.Font = new System.Drawing.Font("Ubuntu", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tryanotherkey.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.tryanotherkey.Location = new System.Drawing.Point(27, 169);
+            this.tryanotherkey.Name = "tryanotherkey";
+            this.tryanotherkey.Size = new System.Drawing.Size(384, 73);
+            this.tryanotherkey.TabIndex = 1;
+            this.tryanotherkey.Text = "Try another key";
+            this.tryanotherkey.UseVisualStyleBackColor = true;
+            this.tryanotherkey.Click += new System.EventHandler(this.tryanotherkey_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.Key);
+            this.panel1.Location = new System.Drawing.Point(27, 116);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(384, 47);
+            this.panel1.TabIndex = 0;
+            // 
+            // Key
+            // 
+            this.Key.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Key.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Key.Font = new System.Drawing.Font("Ubuntu", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Key.ForeColor = System.Drawing.Color.Gainsboro;
+            this.Key.Location = new System.Drawing.Point(3, 10);
+            this.Key.Name = "Key";
+            this.Key.Size = new System.Drawing.Size(378, 25);
+            this.Key.TabIndex = 0;
+            this.Key.Text = "92XBV-29T8P-7989V";
+            this.Key.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.BackgroundImage = global::SteamKeyDeveloper.Properties.Resources.icons8_checked_96px;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Enabled = false;
+            this.pictureBox3.Location = new System.Drawing.Point(95, 1);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(35, 36);
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(437, 270);
+            this.Controls.Add(this.bunifuGradientPanel1);
+            this.Font = new System.Drawing.Font("Ubuntu", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Home";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.bunifuGradientPanel1.ResumeLayout(false);
+            this.bunifuGradientPanel1.PerformLayout();
+            this.feedbackpannel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipseForm;
+        private Bunifu.Framework.UI.BunifuGradientPanel bunifuGradientPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button tryanotherkey;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipsePanel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.TextBox Key;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel feedbackpannel;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer hideFeedback;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SteamKeyDeveloper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        { InitializeComponent(); }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Are you sure you want to exit the application?", "Are you sure?", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (dialogResult == DialogResult.No)
+                return;
+
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        { WindowState = FormWindowState.Minimized; }
+
+        private void tryanotherkey_Click(object sender, EventArgs e)
+        {
+            int desition;
+            string[] Abecedario = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+            string[] Numeros = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+            string output = "";
+            Random rnd = new Random();
+
+
+            
+            //Letra y numero al azar
+            for (int i = 1; i <= 15; i++)
+            {
+                desition = rnd.Next(0, 2);
+                switch (desition)
+                {
+                    case 0:
+                        output = output + Abecedario[rnd.Next(0, Abecedario.Length)];
+                        break;
+
+                    case 1:
+                        output = output + Numeros[rnd.Next(0, Numeros.Length)];
+                        break;
+                }
+
+                if (i == 5 || i == 10)
+                {
+                    output = output + "-";
+                }
+            }
+
+            //Salida
+            Key.Text = output;
+            Clipboard.SetText(output);
+
+            FeedbackOn();
+        }
+
+        private void hideFeedback_Tick(object sender, EventArgs e)
+        { FeedbackOff(); }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { FeedbackOff(); }
+
+        private void FeedbackOn()
+        {
+            feedbackpannel.Visible = true;
+            hideFeedback.Start();
+            hideFeedback.Enabled = true;
+        }
+
+        private void FeedbackOff()
+        {
+            feedbackpannel.Visible = false;
+            hideFeedback.Stop();
+            hideFeedback.Enabled = false;
+        }
+
+        private void feedbackpannel_Click(object sender, EventArgs e)
+        {
+            FeedbackOff();
+        }
+    }
+}
